aeb079767d0c7ece34a471f93db7d90aa7bc7395
Nils Asmussen <nils@os.inf.tu-dresden.de>
2015-08-28 10:30:40 +0200

    gem5: all apps that do not use VPEs seem to work now.

---
+++ src/libs/m3/arch/gem5/VPE.cc
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2013, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel for Minimalist Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+#include <m3/cap/VPE.h>
+
+namespace m3 {
+
+Errors::Code VPE::run(void *lambda) {
+    return Errors::NOT_SUP;
+}
+
+Errors::Code VPE::exec(int argc, const char **argv) {
+    return Errors::NOT_SUP;
+}
+
+}
