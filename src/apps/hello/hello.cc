58ed016d5cee80680736ba0e6278b73c08ef9989
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-03-24 22:01:22 +0100

    Removed useless test applications.

--- src/apps/hello/hello.cc
+++ src/apps/hello/hello.cc
@@ -14,33 +14,11 @@
  * General Public License version 2 for more details.
  */
 
-#include <base/Common.h>
-#include <base/util/Profile.h>
-#include <base/DTU.h>
-
-#include <m3/com/MemGate.h>
 #include <m3/stream/Standard.h>
-#include <m3/vfs/VFS.h>
-#include <m3/vfs/Dir.h>
-#include <m3/Syscalls.h>
 
 using namespace m3;
 
-int main(int argc, char **argv) {
-    cout << "Got " << argc << " arguments:\n";
-    for(int i = 0; i < argc; ++i)
-        cout << "  " << i << ": " << argv[i] << "\n";
-
-    const char *dirname = "/bin";
-    Dir dir(dirname);
-    if(!Errors::occurred()) {
-        cout << "Listing dir " << dirname << "...\n";
-        Dir::Entry e;
-        while(dir.readdir(e))
-            cout << " Found " << e.name << " -> " << e.nodeno << "\n";
-    }
-
-    for(int i = 0; i < 10; ++i)
-        cout << "Hello @ " << Profile::start() << "!\n";
+int main() {
+    cout << "Hello World\n";
     return 0;
 }
