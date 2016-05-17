674e2fa453cac6a2d9ad7fb2c0948a92f7156f1a
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-05-17 16:39:38 +0200

    bench.sh: added a warmup argument to skip the first times.

--- src/tools/bench.sh
+++ src/tools/bench.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <log>" 1>&2
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+    echo "Usage: $0 <log> [<warmup>]" 1>&2
     exit 1
 fi
 
 log=$1
+warmup=0
+if [ "$2" != "" ]; then
+    warmup=$2
+fi
 starttsc="1ff1"
 stoptsc="1ff2"
 
-awk '
+awk -v warmup=$warmup '
 function handle(msg, time) {
     id = substr(msg,7,4)
     if(substr(msg,3,4) == "'$starttsc'") {
         start[id] = time
     }
     else if(substr(msg,3,4) == "'$stoptsc'") {
-        print "TIME:", id, ":", (strtonum(time) - strtonum(start[id])), "cycles"
+        counter[id] += 1
+        if(counter[id] > warmup)
+            print "TIME:", id, ":", (strtonum(time) - strtonum(start[id])), "cycles"
     }
 }
 
