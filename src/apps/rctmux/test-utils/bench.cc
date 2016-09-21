b3b110c30bdf5e2a13a5515c2bc1b97bf4085e67
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-09-21 07:00:44 +0200

    rctmux-bench: do the benchmark multiple times.

--- src/apps/rctmux/test-utils/bench.cc
+++ src/apps/rctmux/test-utils/bench.cc
@@ -30,6 +30,7 @@
 using namespace m3;
 
 #define VERBOSE     0
+#define REPEATS     4
 
 struct App {
     explicit App(int argc, const char *argv[], bool muxed)
@@ -52,61 +53,63 @@ int main(int argc, char **argv) {
     if(VFS::mount("/", new M3FS("m3fs")) < 0)
         PANIC("Cannot mount root fs");
 
-    if(VERBOSE) cout << "Creating VPEs...\n";
-
     bool muxed = strcmp(argv[1], "1") == 0;
     int argcount = IStringStream::read_from<int>(argv[2]);
     App *apps[(argc - 3) / argcount];
 
-    size_t idx = 0;
-    for(int i = 3; i < argc; i += argcount) {
-        const char **args = new const char*[argcount];
-        for(int x = 0; x < argcount; ++x)
-            args[x] = argv[i + x];
-        if(VERBOSE) {
-            cout << "Creating ";
+    for(int j = 0; j < REPEATS; ++j) {
+        if(VERBOSE) cout << "Creating VPEs...\n";
+
+        size_t idx = 0;
+        for(int i = 3; i < argc; i += argcount) {
+            const char **args = new const char*[argcount];
             for(int x = 0; x < argcount; ++x)
-                cout << args[x] << " ";
-            cout << "\n";
+                args[x] = argv[i + x];
+            if(VERBOSE) {
+                cout << "Creating ";
+                for(int x = 0; x < argcount; ++x)
+                    cout << args[x] << " ";
+                cout << "\n";
+            }
+            apps[idx++] = new App(argcount, args, muxed);
         }
-        apps[idx++] = new App(argcount, args, muxed);
-    }
 
-    if(VERBOSE) cout << "Starting VPEs...\n";
+        if(VERBOSE) cout << "Starting VPEs...\n";
 
-    cycles_t start = Profile::start(0x1234);
-
-    for(size_t i = 0; i < ARRAY_SIZE(apps); ++i) {
-        apps[i]->vpe.mountspace(*VPE::self().mountspace());
-        apps[i]->vpe.obtain_mountspace();
-        Errors::Code res = apps[i]->vpe.exec(apps[i]->exec);
-        if(res != Errors::NO_ERROR)
-            PANIC("Cannot execute " << apps[i]->exec.argv()[0] << ": " << Errors::to_string(res));
-
-        if(!muxed) {
-            if(VERBOSE) cout << "Waiting for VPE " << apps[i]->exec.argv()[0] << " ...\n";
-
-            UNUSED int res = apps[i]->vpe.wait();
-            if(VERBOSE) cout << apps[i]->exec.argv()[0] << " exited with " << res << "\n";
-        }
-    }
-
-    if(muxed) {
-        if(VERBOSE) cout << "Waiting for VPEs...\n";
+        cycles_t start = Profile::start(0x1234);
 
         for(size_t i = 0; i < ARRAY_SIZE(apps); ++i) {
-            int res = apps[i]->vpe.wait();
-            if(VERBOSE) cout << apps[i]->exec.argv()[0] << " exited with " << res << "\n";
+            apps[i]->vpe.mountspace(*VPE::self().mountspace());
+            apps[i]->vpe.obtain_mountspace();
+            Errors::Code res = apps[i]->vpe.exec(apps[i]->exec);
+            if(res != Errors::NO_ERROR)
+                PANIC("Cannot execute " << apps[i]->exec.argv()[0] << ": " << Errors::to_string(res));
+
+            if(!muxed) {
+                if(VERBOSE) cout << "Waiting for VPE " << apps[i]->exec.argv()[0] << " ...\n";
+
+                UNUSED int res = apps[i]->vpe.wait();
+                if(VERBOSE) cout << apps[i]->exec.argv()[0] << " exited with " << res << "\n";
+            }
         }
+
+        if(muxed) {
+            if(VERBOSE) cout << "Waiting for VPEs...\n";
+
+            for(size_t i = 0; i < ARRAY_SIZE(apps); ++i) {
+                int res = apps[i]->vpe.wait();
+                if(VERBOSE) cout << apps[i]->exec.argv()[0] << " exited with " << res << "\n";
+            }
+        }
+
+        cycles_t end = Profile::stop(0x1234);
+        cout << "Time: " << (end - start) << "\n";
+
+        if(VERBOSE) cout << "Deleting VPEs...\n";
+
+        for(size_t i = 0; i < ARRAY_SIZE(apps); ++i)
+            delete apps[i];
     }
-
-    cycles_t end = Profile::stop(0x1234);
-    cout << "Time: " << (end - start) << "\n";
-
-    if(VERBOSE) cout << "Deleting VPEs...\n";
-
-    for(size_t i = 0; i < ARRAY_SIZE(apps); ++i)
-        delete apps[i];
 
     if(VERBOSE) cout << "Done\n";
     return 0;
