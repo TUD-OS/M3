cb9e81ebff42cc670a1af402e2bb2b7d8291fb3e
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-02-24 09:57:49 +0100

    Added simple session creation benchmark.

---
+++ src/apps/bench/session/bench-session.cc
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2015, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+#include <base/util/Profile.h>
+
+#include <m3/session/Session.h>
+#include <m3/stream/Standard.h>
+#include <m3/Syscalls.h>
+
+using namespace m3;
+
+static const uint COUNT = 32;
+
+int main() {
+    Syscalls::get().noop();
+
+    cycles_t total = 0;
+
+    for(uint i = 0; i < COUNT; ++i) {
+        cycles_t begin = Profile::start(0x1234);
+        Session sess("test");
+        cycles_t end = Profile::stop(0x1234);
+        total += end - begin;
+    }
+
+    cout << "Per session creation: " << (total / COUNT) << " cycles\n";
+    return 0;
+}
