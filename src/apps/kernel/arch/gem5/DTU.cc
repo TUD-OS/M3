2378ceeb96f0eb1f090dc1d53c10920dbc0168d8
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-03-01 16:28:52 +0100

    kernel: added missing cast.

--- src/apps/kernel/arch/gem5/DTU.cc
+++ src/apps/kernel/arch/gem5/DTU.cc
@@ -307,7 +307,7 @@ void DTU::write_ep_local(epid_t ep) {
 
 void DTU::mark_read_remote(const VPEDesc &vpe, epid_t ep, uintptr_t msg) {
     m3::DTU::reg_t cmd = static_cast<m3::DTU::reg_t>(m3::DTU::ExtCmdOpCode::ACK_MSG);
-    do_ext_cmd(vpe, cmd | (ep << 3) | (msg << 11));
+    do_ext_cmd(vpe, cmd | (ep << 3) | (static_cast<m3::DTU::reg_t>(msg) << 11));
 }
 
 void DTU::drop_msgs(epid_t ep, label_t label) {
