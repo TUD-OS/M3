c74c2602602359e059bbc3997c08ef601336830f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-06-14 21:43:01 +0200

    kernel: fixed warning.

--- src/apps/kernel/arch/host/DTUState.cc
+++ src/apps/kernel/arch/host/DTUState.cc
@@ -94,7 +94,7 @@ void DTUState::config_send(epid_t ep, label_t lbl, peid_t pe, vpeid_t, epid_t ds
 
 void DTUState::config_mem(epid_t ep, peid_t pe, vpeid_t, uintptr_t addr, size_t size, int perms) {
     word_t *regs = reinterpret_cast<word_t*>(get_ep(ep));
-    assert((addr & perms) == 0);
+    assert((addr & static_cast<uintptr_t>(perms)) == 0);
     regs[m3::DTU::EP_LABEL]         = addr | static_cast<uint>(perms);
     regs[m3::DTU::EP_PEID]          = pe;
     regs[m3::DTU::EP_EPID]          = 0;
