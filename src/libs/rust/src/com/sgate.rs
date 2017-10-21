744be5f7ae2ab1b3cf04ab557ca02c0258da8cc5
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-10-21 15:11:06 +0200

    rust: added test for send & reply.

--- src/libs/rust/src/com/sgate.rs
+++ src/libs/rust/src/com/sgate.rs
@@ -96,11 +96,14 @@ impl SendGate {
 
 pub mod tests {
     use super::*;
+    use collections::String;
+    use com;
     use util;
 
     pub fn run(t: &mut ::test::Tester) {
         run_test!(t, create);
         run_test!(t, send_recv);
+        run_test!(t, send_reply);
     }
 
     fn create() {
@@ -132,6 +135,37 @@ pub mod tests {
             let msg = assert_ok!(rgate.wait(Some(&sgate)));
             assert_eq!(msg.header.label, 0x1234);
             dtu::DTU::mark_read(rgate.ep().unwrap(), msg);
+        }
+    }
+
+    fn send_reply() {
+        let reply_gate = RecvGate::def();
+        let mut rgate = assert_ok!(RecvGate::new(util::next_log2(64), util::next_log2(64)));
+        let sgate = assert_ok!(SendGate::new_with(
+            SGateArgs::new(&rgate).credits(64).label(0x1234)
+        ));
+        assert!(sgate.ep().is_none());
+        assert_ok!(rgate.activate());
+
+        assert_ok!(send_vmsg!(&sgate, &reply_gate, 0x123, 12, "test"));
+
+        // sgate -> rgate
+        {
+            let mut msg = assert_ok!(com::recv_msg(&rgate));
+            let (i1, i2, s): (i32, i32, String) = (msg.pop(), msg.pop(), msg.pop());
+            assert_eq!(i1, 0x123);
+            assert_eq!(i2, 12);
+            assert_eq!(s, "test");
+
+            assert_ok!(reply_vmsg!(msg, 44, 3));
+        }
+
+        // rgate -> reply_gate
+        {
+            let mut reply = assert_ok!(com::recv_msg_from(&reply_gate, Some(&sgate)));
+            let (i1, i2): (i32, i32) = (reply.pop(), reply.pop());
+            assert_eq!(i1, 44);
+            assert_eq!(i2, 3);
         }
     }
 }
