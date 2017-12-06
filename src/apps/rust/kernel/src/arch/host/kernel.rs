952fb1c6d12d6a91911b11f35457be0f0927c066
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-12-06 15:58:54 +0100

    rustkernel: load/store FS image on host.

--- src/apps/rust/kernel/src/arch/host/kernel.rs
+++ src/apps/rust/kernel/src/arch/host/kernel.rs
@@ -1,3 +1,5 @@
+use base::cfg;
+use base::col::ToString;
 use base::dtu;
 use base::env;
 use base::envdata;
@@ -5,6 +7,7 @@ use base::heap;
 use base::io;
 use base::kif;
 use base::libc;
+use core::intrinsics;
 use thread;
 
 use arch::kdtu::KDTU;
@@ -43,11 +46,64 @@ pub extern "C" fn rust_deinit(_status: i32, _arg: *const libc::c_void) {
     dtu::deinit();
 }
 
+fn copy_from_fs(path: &str) -> usize {
+    unsafe {
+        let fd = libc::open(path.as_bytes().as_ptr() as *const i8, libc::O_RDONLY);
+        assert!(fd != -1);
+
+        let mut info: libc::stat = intrinsics::uninit();
+        assert!(libc::fstat(fd, &mut info) != -1);
+        assert!(info.st_size as usize <= cfg::FS_MAX_SIZE);
+
+        let alloc = mem::get().allocate_at(0, cfg::FS_MAX_SIZE)
+            .expect("Unable to alloc space for FS image");
+
+        let res = libc::read(
+            fd,
+            alloc.global().offset() as *mut libc::c_void,
+            info.st_size as usize
+        );
+        assert!(res == info.st_size as isize);
+
+        libc::close(fd);
+
+        let fs_size = res as usize;
+        klog!(MEM, "Copied fs-image '{}' to 0..{:#x}", path, fs_size);
+        fs_size
+    }
+}
+
+fn copy_to_fs(path: &str, fs_size: usize) {
+    let out_path = path.to_string() + ".out";
+
+    unsafe {
+        let fd = libc::open(
+            out_path.as_bytes().as_ptr() as *const i8,
+            libc::O_WRONLY | libc::O_TRUNC | libc::O_CREAT,
+            0o600
+        );
+        assert!(fd != -1);
+
+        let alloc = mem::get().allocate_at(0, cfg::FS_MAX_SIZE)
+            .expect("Unable to alloc space for FS image");
+
+        libc::write(fd, alloc.global().offset() as *const libc::c_void, fs_size);
+        libc::close(fd);
+    }
+
+    klog!(MEM, "Copied fs-image back to '{}'", out_path);
+}
+
 #[no_mangle]
 pub fn main() -> i32 {
-    if let Some(a) = env::args().nth(1) {
-        if a == "test" {
+    let mut fs_image: Option<&str> = None;
+
+    for arg in env::args() {
+        if arg == "test" {
             tests::run();
+        }
+        if arg.len() > 3 && &arg[0..3] == "fs=" {
+            fs_image = Some(&arg[3..]);
         }
     }
 
@@ -62,6 +118,8 @@ pub fn main() -> i32 {
     loader::init();
     pes::vpemng::init();
     thread::init();
+
+    let fs_size = if let Some(path) = fs_image { copy_from_fs(path) } else { 0 };
 
     for _ in 0..8 {
         thread::ThreadManager::get().add_thread(workloop as *const () as u64, 0);
@@ -81,6 +139,10 @@ pub fn main() -> i32 {
     klog!(DEF, "Kernel is ready!");
 
     workloop();
+
+    if let Some(path) = fs_image {
+        copy_to_fs(path, fs_size);
+    }
 
     klog!(DEF, "Shutting down");
     0
