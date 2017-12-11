8af49ec7a360d00b80ea49039afb7a805da8ec9c
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-12-11 21:41:01 +0100

    rustbase: documented StaticCell.

--- src/libs/rustbase/src/cell/mod.rs
+++ src/libs/rustbase/src/cell/mod.rs
@@ -6,6 +6,10 @@ use core::ops::Deref;
 
 pub use core::cell::{Cell, Ref, RefCell, RefMut, UnsafeCell};
 
+/// A cell that allows to mutate a static immutable object in single threaded environments
+///
+/// Since M3 does not support multiple threads within one address space, mutable static objects
+/// are perfectly fine. Thus, use this wrapper to convince rust of that.
 pub struct StaticCell<T: Sized> {
     inner: UnsafeCell<T>,
 }
@@ -13,23 +17,27 @@ pub struct StaticCell<T: Sized> {
 unsafe impl<T: Sized> Sync for StaticCell<T> {}
 
 impl<T: Sized> StaticCell<T> {
+    /// Creates a new static cell with given value
     pub const fn new(val: T) -> Self {
         StaticCell {
             inner: UnsafeCell::new(val),
         }
     }
 
+    /// Returns a reference to the inner value
     pub fn get(&self) -> &T {
         unsafe {
             &*self.inner.get()
         }
     }
+    /// Returns a mutable reference to the inner value
     pub fn get_mut(&self) -> &mut T {
         unsafe {
             &mut *self.inner.get()
         }
     }
 
+    /// Sets the inner value to `val`
     pub fn set(&self, val: T) {
         mem::replace(self.get_mut(), val);
     }
