c758ace1a2d58cbb45a3dd20d6082314de7f0bbf
Nils Asmussen <nils@script-solution.de>
2018-01-10 13:46:49 +0100

    Updated pagefault benchmark.

--- src/apps/bench/pagefaults/pagefaults.cc
+++ src/apps/bench/pagefaults/pagefaults.cc
@@ -24,56 +24,73 @@
 
 using namespace m3;
 
-static const size_t COUNT       = 8;
+static const size_t COUNT       = 4;
 static const size_t PAGES       = 64;
 
-static void do_bench(volatile char *data) {
+static cycles_t do_warmup(volatile char *data, unsigned id1) {
+    cycles_t start = Profile::start(id1);
     for(size_t i = 0; i < PAGES; ++i)
         data[i * PAGE_SIZE] = i;
+    return Profile::stop(id1) - start;
 }
 
-int main() {
+static cycles_t do_bench(volatile char *data, unsigned id1, unsigned id2) {
+    cycles_t start = Profile::start(id1);
+    for(size_t i = 0; i < PAGES; ++i) {
+        Profile::start(id2);
+        data[i * PAGE_SIZE] = i;
+        Profile::stop(id2);
+    }
+    return Profile::stop(id1) - start;
+}
+
+int main(int argc, char **argv) {
+    if(argc < 2)
+        exitmsg("Usage: " << argv[0] << " (anon|file)");
     if(!VPE::self().pager())
         exitmsg("No pager");
 
-    cycles_t anon = 0;
-    for(size_t i = 0; i < COUNT; ++i) {
-        uintptr_t virt = 0x30000000;
-        Errors::Code res = VPE::self().pager()->map_anon(&virt, PAGES * PAGE_SIZE,
-            Pager::READ | Pager::WRITE, 0);
-        if(res != Errors::NONE)
-            exitmsg("Unable to map anonymous memory");
+    if(strcmp(argv[1], "anon") == 0) {
+        cycles_t anon = 0;
+        for(size_t i = 0; i < COUNT; ++i) {
+            uintptr_t virt = 0x30000000;
+            Errors::Code res = VPE::self().pager()->map_anon(&virt, PAGES * PAGE_SIZE,
+                Pager::READ | Pager::WRITE, 0);
+            if(res != Errors::NONE)
+                exitmsg("Unable to map anonymous memory");
 
-        cycles_t start = Profile::start(0);
-        do_bench(reinterpret_cast<char*>(virt));
-        cycles_t end = Profile::stop(0);
-        anon += end - start;
+            if(i < COUNT - 1)
+                anon += do_warmup(reinterpret_cast<char*>(virt), 0xFF);
+            else
+                anon += do_bench(reinterpret_cast<char*>(virt), 0xFF, 0);
 
-        VPE::self().pager()->unmap(virt);
+            VPE::self().pager()->unmap(virt);
+        }
+        cout << "anon: " << (anon / (COUNT * PAGES)) << "\n";
     }
 
-    cycles_t file = 0;
-    for(size_t i = 0; i < COUNT; ++i) {
-        FileRef f("/zeros.bin", FILE_RW);
-        if(Errors::last != Errors::NONE)
-            exitmsg("Unable to open /zeros.bin");
+    if(strcmp(argv[1], "file") == 0) {
+        cycles_t file = 0;
+        for(size_t i = 0; i < COUNT; ++i) {
+            FileRef f("/zeros.bin", FILE_RW);
+            if(Errors::last != Errors::NONE)
+                exitmsg("Unable to open /zeros.bin");
 
-        RegularFile *rfile = static_cast<RegularFile*>(&*f);
-        uintptr_t virt = 0x31000000;
-        Errors::Code res = VPE::self().pager()->map_ds(&virt, PAGES * PAGE_SIZE,
-            Pager::READ | Pager::WRITE, 0, *rfile->fs(), rfile->fd(), 0);
-        if(res != Errors::NONE)
-            exitmsg("Unable to map /test.txt");
+            RegularFile *rfile = static_cast<RegularFile*>(&*f);
+            uintptr_t virt = 0x31000000;
+            Errors::Code res = VPE::self().pager()->map_ds(&virt, PAGES * PAGE_SIZE,
+                Pager::READ | Pager::WRITE, 0, *rfile->fs(), rfile->fd(), 0);
+            if(res != Errors::NONE)
+                exitmsg("Unable to map /test.txt");
 
-        cycles_t start = Profile::start(1);
-        do_bench(reinterpret_cast<char*>(virt));
-        cycles_t end = Profile::stop(1);
-        file += end - start;
+            if(i < COUNT - 1)
+                file += do_warmup(reinterpret_cast<char*>(virt), 0xFF);
+            else
+                file += do_bench(reinterpret_cast<char*>(virt), 0xFF, 1);
 
-        VPE::self().pager()->unmap(virt);
+            VPE::self().pager()->unmap(virt);
+        }
+        cout << "file: " << (file / (COUNT * PAGES)) << "\n";
     }
-
-    cout << "anon: " << (anon / COUNT) << "\n";
-    cout << "file: " << (file / COUNT) << "\n";
     return 0;
 }
