3c9c5125bacfb230192e22d50ac03a5cd2360317
Nils Asmussen <nils@os.inf.tu-dresden.de>
2018-01-13 21:34:41 +0100

    rusthello: removed tests.

--- src/apps/rust/hello/src/rusthello.rs
+++ src/apps/rust/hello/src/rusthello.rs
@@ -7,15 +7,5 @@ extern crate m3;
 pub fn main() -> i32 {
     println!("Hello world!");
 
-    let mgate1 = m3::com::MemGate::new(0x1000, m3::kif::Perm::RW).unwrap();
-    println!("{:?}", mgate1);
-
-    let start = m3::time::start(0x666);
-    let mgate = m3::com::MemGate::new(0x1000, m3::kif::Perm::RW).unwrap();
-    let data = [0xFFu8; 10];
-    mgate.write(&data, 0).expect("Write failed");
-    let end = m3::time::stop(0x666);
-    println!("Time: {} for {:?}", end - start, mgate);
-
     0
 }
