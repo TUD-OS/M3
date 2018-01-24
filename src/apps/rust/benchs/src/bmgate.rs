ab44d44ffa187b3d8577e6b701869845f8a5ae8c
Nils Asmussen <nils@os.inf.tu-dresden.de>
2018-01-24 15:18:00 +0100

    rustbench: read/write 2 MiB in MemGate test.

--- src/apps/rust/benchs/src/bmgate.rs
+++ src/apps/rust/benchs/src/bmgate.rs
@@ -3,6 +3,8 @@ use m3::kif;
 use m3::profile;
 use m3::test;
 
+const SIZE: usize = 2 * 1024 * 1024;
+
 pub fn run(t: &mut test::Tester) {
     run_test!(t, read);
     run_test!(t, write);
@@ -12,10 +14,14 @@ fn read() {
     let mut buf = vec![0u8; 8192];
     let mgate = MemGate::new(8192, kif::Perm::R).expect("Unable to create mgate");
 
-    let mut prof = profile::Profiler::new();
+    let mut prof = profile::Profiler::new().repeats(2).warmup(1);
 
-    println!("8K: {}", prof.run_with_id(|| {
-        mgate.read(&mut buf, 0).expect("Reading failed");
+    println!("2 MiB with 8K buf: {}", prof.run_with_id(|| {
+        let mut total = 0;
+        while total < SIZE {
+            mgate.read(&mut buf, 0).expect("Reading failed");
+            total += buf.len();
+        }
     }, 0x30));
 }
 
@@ -23,9 +29,13 @@ fn write() {
     let buf = vec![0u8; 8192];
     let mgate = MemGate::new(8192, kif::Perm::W).expect("Unable to create mgate");
 
-    let mut prof = profile::Profiler::new();
+    let mut prof = profile::Profiler::new().repeats(2).warmup(1);
 
-    println!("8K: {}", prof.run_with_id(|| {
-        mgate.write(&buf, 0).expect("Writing failed");
+    println!("2 MiB with 8K buf: {}", prof.run_with_id(|| {
+        let mut total = 0;
+        while total < SIZE {
+            mgate.write(&buf, 0).expect("Writing failed");
+            total += buf.len();
+        }
     }, 0x31));
 }
