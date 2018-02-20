5f80145dae7b613e48288a78b8f6d16bb107b902
Nils Asmussen <nils@os.inf.tu-dresden.de>
2018-02-20 13:22:12 +0100

    aladdin: improvements.

--- src/apps/aladdin/aladdin.cc
+++ src/apps/aladdin/aladdin.cc
@@ -15,6 +15,9 @@
  */
 
 #include <base/Common.h>
+#include <base/stream/Serial.h>
+#include <base/stream/IStringStream.h>
+#include <base/CmdArgs.h>
 
 #include <m3/stream/Standard.h>
 
@@ -22,9 +25,15 @@
 #include <m3/com/RecvGate.h>
 #include <m3/com/SendGate.h>
 #include <m3/session/Pager.h>
+#include <m3/vfs/RegularFile.h>
+#include <m3/vfs/VFS.h>
 #include <m3/VPE.h>
 
 using namespace m3;
+
+static int step_size = 100; // percent
+static bool use_files = false;
+static bool map_eager = false;
 
 class Aladdin {
 public:
@@ -40,11 +49,13 @@ public:
     static const size_t STATE_ADDR  = BUF_ADDR - STATE_SIZE;
     static const size_t RBUF_ADDR   = RECVBUF_SPACE + SYSC_RBUF_SIZE + UPCALL_RBUF_SIZE + DEF_RBUF_SIZE;
 
+    struct Array {
+        uint64_t addr;
+        uint64_t size;
+    } PACKED;
+
     struct InvokeMessage {
-        struct {
-            uint64_t addr;
-            uint64_t size;
-        } PACKED arrays[8];
+        Array arrays[8];
         uint64_t array_count;
         uint64_t iterations;
     } PACKED;
@@ -80,7 +91,7 @@ public:
 
     void run(InvokeMessage &msg, size_t iterations) {
         size_t count = 0;
-        size_t per_step = 1;
+        size_t per_step = iterations * ((float)step_size / 100);
         while(count < iterations) {
             msg.iterations = Math::min(iterations - count, per_step);
             invoke(msg);
@@ -95,11 +106,61 @@ public:
     m3::SendGate _sgate;
 };
 
-int main(int argc, char **argv) {
-    if(argc != 2)
-        exitmsg("Usage: " << argv[0] << " (stencil|md|spmv|fft)");
+static void add(Aladdin &alad, size_t size, Aladdin::Array *a, int prot) {
+    static uintptr_t virt = 0x1000000;
+    size_t psize = Math::round_up(size, PAGE_SIZE);
 
-    if(strcmp(argv[1], "stencil") == 0) {
+    if(use_files) {
+        int perms = (prot & MemGate::W) ? FILE_RW : FILE_R;
+        fd_t fd = VFS::open("/zeros.bin", perms);
+        RegularFile *file = static_cast<RegularFile*>(VPE::self().fds()->get(fd));
+        int flags = (prot & MemGate::W) ? Pager::MAP_SHARED : Pager::MAP_PRIVATE;
+        alad._accel->pager()->map_ds(&virt, psize, prot, flags, *file->fs(), file->fd(), 0);
+    }
+    else {
+        MemGate *mem = new MemGate(MemGate::create_global(psize, prot));
+        alad._accel->pager()->map_mem(&virt, *mem, psize, prot);
+    }
+
+    if(map_eager) {
+        size_t off = 0;
+        size_t pages = psize / PAGE_SIZE;
+        while(pages > 0) {
+            alad._accel->pager()->pagefault(virt + off, static_cast<uint>(prot));
+            pages -= 1;
+            off += PAGE_SIZE;
+        }
+    }
+
+    a->addr = virt;
+    a->size = size;
+    virt += psize;
+}
+
+static void usage(const char *name) {
+    Serial::get() << "Usage: " << name << " [-s <step_size>] [-f] [-e] (stencil|md|spmv|fft)\n";
+    Serial::get() << "  -s: the step size in percent\n";
+    Serial::get() << "  -f: use files for input and output\n";
+    Serial::get() << "  -e: map all memory eagerly\n";
+    exit(1);
+}
+
+int main(int argc, char **argv) {
+    int opt;
+    while((opt = CmdArgs::get(argc, argv, "s:fe")) != -1) {
+        switch(opt) {
+            case 's': step_size = IStringStream::read_from<int>(CmdArgs::arg); break;
+            case 'f': use_files = true; break;
+            case 'e': map_eager = true; break;
+            default:
+                usage(argv[0]);
+        }
+    }
+    if(CmdArgs::ind >= argc || step_size <= 0 || step_size > 100)
+        usage(argv[0]);
+
+    const char *bench = argv[CmdArgs::ind];
+    if(strcmp(bench, "stencil") == 0) {
         Aladdin alad(PEISA::ACCEL_STE);
 
         const size_t HEIGHT = 32;
@@ -115,81 +176,57 @@ int main(int argc, char **argv) {
 
         Aladdin::InvokeMessage msg;
         msg.array_count = 3;
-        msg.arrays[0].addr = dataAddr;            msg.arrays[0].size = SIZE;
-        msg.arrays[1].addr = dataAddr + SIZE;     msg.arrays[1].size = SIZE;
-        msg.arrays[2].addr = filAddr;             msg.arrays[2].size = 8;
+        add(alad, SIZE, msg.arrays + 0, MemGate::R);                        // orig
+        add(alad, SIZE, msg.arrays + 1, MemGate::W);                        // sol
+        add(alad, 8, msg.arrays + 2, MemGate::R);                           // C
 
         alad.run(msg, ITERATIONS);
     }
-    else if(strcmp(argv[1], "md") == 0) {
+    else if(strcmp(bench, "md") == 0) {
         Aladdin alad(PEISA::ACCEL_MD);
 
         const size_t ATOMS = 4096;
         const size_t MAX_NEIGHBORS = 16;
         const size_t ATOM_SET = ATOMS * sizeof(double);
 
-        uintptr_t posAddr = 0x1000000;
-        alad._accel->pager()->map_anon(&posAddr, ATOM_SET * 3, Pager::Prot::RW, 0);
-        uintptr_t forceAddr = 0x2000000;
-        alad._accel->pager()->map_anon(&forceAddr, ATOM_SET * 3, Pager::Prot::RW, 0);
-        uintptr_t neighAddr = 0x3000000;
-        alad._accel->pager()->map_anon(&neighAddr, ATOMS * MAX_NEIGHBORS * 4, Pager::Prot::RW, 0);
-
         Aladdin::InvokeMessage msg;
         msg.array_count = 7;
-        for(size_t i = 0; i < 3; ++i) {
-            msg.arrays[i].addr = posAddr + ATOM_SET * i;
-            msg.arrays[i].size = ATOM_SET;
-        }
-        for(size_t i = 0; i < 3; ++i) {
-            msg.arrays[3 + i].addr = forceAddr + ATOM_SET * i;
-            msg.arrays[3 + i].size = ATOM_SET;
-        }
-        msg.arrays[6].addr = neighAddr;
-        msg.arrays[6].size = ATOMS * MAX_NEIGHBORS * 4;
+        for(size_t i = 0; i < 3; ++i)
+            add(alad, ATOM_SET, msg.arrays + i, MemGate::R);                // position_{x,y,z}
+        for(size_t i = 0; i < 3; ++i)
+            add(alad, ATOM_SET, msg.arrays + 3 + i, MemGate::W);            // force_{x,y,z}
+        add(alad, ATOMS * MAX_NEIGHBORS * 4, msg.arrays + 6, MemGate::R);   // NC
 
         alad.run(msg, ATOMS);
     }
-    else if(strcmp(argv[1], "spmv") == 0) {
+    else if(strcmp(bench, "spmv") == 0) {
         Aladdin alad(PEISA::ACCEL_SPMV);
 
         const size_t NNZ = 39321;
         const size_t N = 2048;
         const size_t VEC_LEN = N * sizeof(double);
 
-        uintptr_t valAddr = 0x1000000;
-        alad._accel->pager()->map_anon(&valAddr, NNZ * sizeof(double), Pager::Prot::RW, 0);
-        uintptr_t colsAddr = 0x2000000;
-        alad._accel->pager()->map_anon(&colsAddr, NNZ * sizeof(int32_t), Pager::Prot::RW, 0);
-        uintptr_t rowDelimAddr = 0x3000000;
-        alad._accel->pager()->map_anon(&rowDelimAddr, (N + 1) * sizeof(int32_t), Pager::Prot::RW, 0);
-        uintptr_t vecOutAddr = 0x4000000;
-        alad._accel->pager()->map_anon(&vecOutAddr, VEC_LEN * 2, Pager::Prot::RW, 0);
-
         Aladdin::InvokeMessage msg;
         msg.array_count = 5;
-        msg.arrays[0].addr = valAddr;               msg.arrays[0].size = NNZ * sizeof(double);
-        msg.arrays[1].addr = colsAddr;              msg.arrays[1].size = NNZ * sizeof(int32_t);
-        msg.arrays[2].addr = rowDelimAddr;          msg.arrays[2].size = (N + 1) * sizeof(int32_t);
-        msg.arrays[3].addr = vecOutAddr;            msg.arrays[3].size = VEC_LEN;
-        msg.arrays[4].addr = vecOutAddr + VEC_LEN;  msg.arrays[4].size = VEC_LEN;
+        add(alad, NNZ * sizeof(double), msg.arrays + 0, MemGate::R);        // val
+        add(alad, NNZ * sizeof(int32_t), msg.arrays + 1, MemGate::R);       // cols
+        add(alad, (N + 1) * sizeof(int32_t), msg.arrays + 2, MemGate::R);   // rowDelimiters
+        add(alad, VEC_LEN, msg.arrays + 3, MemGate::R);                     // vec
+        add(alad, VEC_LEN, msg.arrays + 4, MemGate::W);                     // out
 
         alad.run(msg, N);
     }
-    else if(strcmp(argv[1], "fft") == 0) {
+    else if(strcmp(bench, "fft") == 0) {
         Aladdin alad(PEISA::ACCEL_AFFT);
 
         const size_t DATA_LEN = 16384;
         const size_t SIZE = DATA_LEN * sizeof(double);
         const size_t ITERS = (DATA_LEN / 512) * 11;
 
-        uintptr_t dataAddr = 0x1000000;
-        alad._accel->pager()->map_anon(&dataAddr, SIZE * 2, Pager::Prot::RW, 0);
-
         Aladdin::InvokeMessage msg;
         msg.array_count = 2;
-        msg.arrays[0].addr = dataAddr;               msg.arrays[0].size = SIZE;
-        msg.arrays[1].addr = dataAddr + SIZE;        msg.arrays[1].size = SIZE;
+        add(alad, SIZE, msg.arrays + 0, MemGate::RW);                       // work_x
+        add(alad, SIZE, msg.arrays + 1, MemGate::RW);                       // work_y
 
         alad.run(msg, ITERS);
     }
