f14a66b4036b8ccfef44f178bea355b64d8fb95e
Nils Asmussen <nils@os.inf.tu-dresden.de>
2018-05-06 18:23:20 +0200

    rctmux: added aladdin and stream test.

---
+++ src/apps/rctmux/test-utils/aladdintest.cc
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2015, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+#include <base/Common.h>
+#include <base/stream/Serial.h>
+#include <base/stream/IStringStream.h>
+#include <base/util/Time.h>
+#include <base/CmdArgs.h>
+
+#include <m3/accel/AladdinAccel.h>
+#include <m3/com/MemGate.h>
+#include <m3/com/RecvGate.h>
+#include <m3/com/SendGate.h>
+#include <m3/session/Pager.h>
+#include <m3/stream/Standard.h>
+#include <m3/vfs/VFS.h>
+#include <m3/VPE.h>
+
+using namespace m3;
+
+static const int REPEATS = 10;
+
+static void add(Aladdin &alad, uintptr_t *virt, size_t size, Aladdin::Array *a, int prot) {
+    size_t psize = Math::round_up(size, PAGE_SIZE);
+
+    MemGate *mem = new MemGate(MemGate::create_global(psize, prot));
+    alad._accel->pager()->map_mem(virt, *mem, psize, prot);
+
+    size_t off = 0;
+    size_t pages = psize / PAGE_SIZE;
+    while(pages > 0) {
+        alad._accel->pager()->pagefault(*virt + off, static_cast<uint>(prot));
+        pages -= 1;
+        off += PAGE_SIZE;
+    }
+
+    a->addr = *virt;
+    a->size = size;
+    *virt += psize;
+}
+
+int main() {
+    Aladdin alad1(PEISA::ACCEL_STE);
+    Aladdin alad2(PEISA::ACCEL_STE);
+
+    const size_t HEIGHT = 32;
+    const size_t COLS = 32;
+    const size_t ROWS = 64;
+    const size_t SIZE = HEIGHT * ROWS * COLS * sizeof(uint32_t);
+
+    Aladdin::InvokeMessage msg;
+    msg.array_count = 3;
+    msg.iterations = 1;
+
+    goff_t virt = 0x1000000;
+    add(alad1, &virt, SIZE, msg.arrays + 0, MemGate::R);                        // orig
+    add(alad1, &virt, SIZE, msg.arrays + 1, MemGate::W);                        // sol
+    add(alad1, &virt, 8, msg.arrays + 2, MemGate::R);                           // C
+
+    virt = 0x1000000;
+    add(alad2, &virt, SIZE, msg.arrays + 0, MemGate::R);                        // orig
+    add(alad2, &virt, SIZE, msg.arrays + 1, MemGate::W);                        // sol
+    add(alad2, &virt, 8, msg.arrays + 2, MemGate::R);                           // C
+
+    cycles_t total = 0;
+    for(int i = 0; i < REPEATS; ++i) {
+        alad1.invoke(msg);
+
+        cycles_t start = Time::start(0x1234);
+        alad2.invoke(msg);
+        cycles_t end = Time::stop(0x1234);
+        total += end - start;
+    }
+
+    cout << "Time per call: " << (total / REPEATS) << " cycles\n";
+    return 0;
+}
---
+++ src/apps/rctmux/test-utils/streamtest.cc
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2015, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+#include <base/util/Time.h>
+
+#include <m3/accel/StreamAccel.h>
+#include <m3/stream/Standard.h>
+#include <m3/pipe/IndirectPipe.h>
+#include <m3/vfs/VFS.h>
+#include <m3/Syscalls.h>
+
+using namespace m3;
+
+static constexpr int REPEATS            = 20;
+static constexpr cycles_t COMP_TIME     = 1024;
+
+int main() {
+    VPE *vpes[2];
+    StreamAccel *accels[2];
+    SendGate *ins[2];
+    SendGate *outs[2];
+    RecvGate *rgates[2];
+
+    // create VPEs
+    for(size_t i = 0; i < 2; ++i) {
+        OStringStream name;
+        name << "chain" << i;
+
+        vpes[i] = new VPE(name.str(), PEDesc(PEType::COMP_IMEM, PEISA::ACCEL_FFT), nullptr, true);
+        if(Errors::last != Errors::NONE) {
+            exitmsg("Unable to create VPE for " << name.str());
+            break;
+        }
+
+        accels[i] = new StreamAccel(vpes[i], COMP_TIME);
+    }
+
+    // create and activate gates
+    for(size_t i = 0; i < 2; ++i) {
+        rgates[i] = new RecvGate(RecvGate::create(nextlog2<64 * 2>::val, nextlog2<64>::val));
+        rgates[i]->activate();
+
+        ins[i] = new SendGate(SendGate::create(rgates[i], StreamAccel::LBL_IN_REQ, 64));
+        ins[i]->activate_for(*vpes[i], StreamAccel::EP_IN_SEND);
+
+        outs[i] = new SendGate(SendGate::create(rgates[i], StreamAccel::LBL_OUT_REQ, 64));
+        outs[i]->activate_for(*vpes[i], StreamAccel::EP_OUT_SEND);
+    }
+
+    // start VPEs
+    for(size_t i = 0; i < 2; ++i)
+        vpes[i]->start();
+
+    cycles_t start, end, total = 0;
+    for(int i = 0; i < REPEATS; ++i) {
+        {
+            GateIStream msg = receive_msg(*rgates[i % 2]);
+            assert(msg.label<label_t>() == StreamAccel::LBL_IN_REQ);
+
+            start = Time::start(0x1234);
+            reply_vmsg(msg, Errors::NONE, static_cast<uint64_t>(0), static_cast<uint64_t>(8));
+        }
+
+        {
+            GateIStream msg = receive_msg(*rgates[i % 2]);
+            assert(msg.label<label_t>() == StreamAccel::LBL_OUT_REQ);
+            end = Time::stop(0x1234);
+
+            reply_vmsg(msg, Errors::NONE, static_cast<uint64_t>(0), static_cast<uint64_t>(8));
+        }
+
+        total += end - start;
+    }
+
+    for(int i = 0; i < 2; ++i) {
+        GateIStream msg = receive_msg(*rgates[i]);
+        assert(msg.label<label_t>() == StreamAccel::LBL_IN_REQ);
+        reply_vmsg(msg, Errors::NONE, static_cast<uint64_t>(0), static_cast<uint64_t>(0));
+    }
+
+    vpes[0]->wait();
+    vpes[1]->wait();
+
+    cout << "Total time: " << (total / REPEATS) << " cycles\n";
+
+    for(size_t i = 0; i < 2; ++i) {
+        delete vpes[i];
+        delete accels[i];
+        delete ins[i];
+        delete outs[i];
+        delete rgates[i];
+    }
+
+    return 0;
+}
