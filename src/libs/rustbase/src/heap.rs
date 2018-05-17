0559fe3a143580aa6a0c2c3b4bea4767e05e03d8
Nils Asmussen <nils@os.inf.tu-dresden.de>
2018-05-17 19:43:40 +0200

    rust: init large kernel heap on PE type C.

--- src/libs/rustbase/src/heap.rs
+++ src/libs/rustbase/src/heap.rs
@@ -75,6 +75,9 @@ fn init_heap() {
         let end = if env.heap_size == 0 {
             PEDesc::new_from(env.pe_desc).mem_size() - cfg::RECVBUF_SIZE_SPM
         }
+        else if PEDesc::new_from(env.pe_desc).has_mmu() && env.pe_id == 0 {
+            util::round_up(begin as usize, cfg::PAGE_SIZE) + 1024 * 1024
+        }
         else {
             util::round_up(begin as usize, cfg::PAGE_SIZE) + env.heap_size as usize
         };
