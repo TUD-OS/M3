f2edbff539d65538a558584b6cefc213c608302f
Nils Asmussen <nils@script-solution.de>
2018-07-21 15:07:55 +0200

    kernel: fixed bug in VPE exit.

--- src/apps/kernel/pes/VPEGroup.cc
+++ src/apps/kernel/pes/VPEGroup.cc
@@ -29,7 +29,7 @@ VPEGroup::~VPEGroup() {
 
 bool VPEGroup::all_yielded() const {
     for(auto it = vpes.begin(); it != vpes.end(); ++it) {
-        if(!it->vpe->has_yielded())
+        if(it->vpe->has_app() && !it->vpe->has_yielded())
             return false;
     }
     return true;
