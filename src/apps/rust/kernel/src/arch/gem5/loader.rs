4687d3e7020d9d4061010f4ed7ff6a3ebc8e9a25
Nils Asmussen <nils@os.inf.tu-dresden.de>
2018-12-09 14:37:33 +0100

    rust: added copyright notices.

--- src/apps/rust/kernel/src/arch/gem5/loader.rs
+++ src/apps/rust/kernel/src/arch/gem5/loader.rs
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2018, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
 use base::cfg::{MOD_HEAP_SIZE, RT_START, STACK_TOP, PAGE_BITS, PAGE_MASK, PAGE_SIZE};
 use base::cfg::{RECVBUF_SPACE, RECVBUF_SIZE};
 use base::cell::{Cell, StaticCell};
--- src/libs/rustbase/src/lib.rs
+++ src/libs/rustbase/src/lib.rs
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2018, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
 #![feature(alloc, alloc_error_handler, allocator_internals)]
 #![feature(asm)]
 #![feature(box_into_raw_non_null)]
